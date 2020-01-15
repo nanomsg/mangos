07feed44000924099b1ad5bf044819b282643099
Garrett D'Amore <garrett@damore.org>
2020-01-14 20:13:25 -0800

    Update the file copyright.

--- protocol/req/req.go
+++ protocol/req/req.go
@@ -1,4 +1,4 @@
-// Copyright 2019 The Mangos Authors
+// Copyright 2020 The Mangos Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use file except in compliance with the License.
